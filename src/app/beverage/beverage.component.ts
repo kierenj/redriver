3e98615d4b7c660442854517a71b4940781c6d6c
AndreaLiew27 <73441685+AndreaLiew27@users.noreply.github.com>
2020-11-15 15:56:53 +0000

    Coffee Machine

---
+++ src/app/beverage/beverage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beverage',
+  templateUrl: './beverage.component.html',
+  styleUrls: ['./beverage.component.scss']
+})
+export class BeverageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/model/beverage.model.ts
@@ -0,0 +1,5 @@
+export interface Beverage {
+	LemonTea: string;
+	Coffee: string;
+	Chocolate: string;
+}
